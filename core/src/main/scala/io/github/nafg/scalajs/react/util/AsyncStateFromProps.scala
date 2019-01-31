02d5f92851532c54adb25f62b2b661e91717c0a7
Naftoli Gugenheim <naftoligug@gmail.com>
2019-01-31 16:32:40 -0500

    Set version to 0.7.0

--- core/src/main/scala/io/github/nafg/scalajs/react/util/AsyncStateFromProps.scala
+++ core/src/main/scala/io/github/nafg/scalajs/react/util/AsyncStateFromProps.scala
@@ -2,7 +2,7 @@ package io.github.nafg.scalajs.react.util
 
 import scala.concurrent.{ExecutionContext, Future}
 
-import japgolly.scalajs.react.component.builder.Builder.Config
+import japgolly.scalajs.react.component.builder.Builder.{Config, Step4}
 import japgolly.scalajs.react.component.builder.Lifecycle.StateRW
 import japgolly.scalajs.react.{Callback, Children, UpdateSnapshot}
 
@@ -33,5 +33,31 @@ object AsyncStateFromProps {
       .componentWillReceiveProps { self =>
         Callback.when(predicate(self.currentProps, self.nextProps))(run(self.nextProps, self))
       }
+  }
+
+  implicit class ext[P, C <: Children, S, B, US <: UpdateSnapshot](self: Step4[P, C, S, B, US]) {
+    object asyncStateFromProps {
+      def apply(predicate: (P, P) => Boolean, compute: (StateRW[P, S, B], P) => Future[S => S])
+               (implicit executionContext: ExecutionContext): Step4[P, C, S, B, US] = {
+        val run: (P, StateRW[P, S, B]) => Callback =
+          (props, self) => Callback.future(compute(self, props).map(self.modState))
+        self
+          .componentDidMount(self => run(self.props, self))
+          .componentWillReceiveProps { self =>
+            Callback.when(predicate(self.currentProps, self.nextProps))(run(self.nextProps, self))
+          }
+      }
+
+      def always(compute: (StateRW[P, S, B], P) => Future[S => S])
+                (implicit executionContext: ExecutionContext): Step4[P, C, S, B, US] = apply((_, _) => true, compute)
+
+      def const(predicate: (P, P) => Boolean, compute: (StateRW[P, S, B], P) => Future[S])
+               (implicit executionContext: ExecutionContext): Step4[P, C, S, B, US] =
+        apply(predicate, compute(_, _).map(Function.const(_)))
+
+      def constAlways(compute: (StateRW[P, S, B], P) => Future[S])
+                     (implicit executionContext: ExecutionContext): Step4[P, C, S, B, US] =
+        apply((_, _) => true, compute(_, _).map(Function.const(_)))
+    }
   }
 }
