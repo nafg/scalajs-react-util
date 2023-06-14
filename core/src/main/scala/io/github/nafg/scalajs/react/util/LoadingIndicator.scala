706c796e790bcb4c91871b679661e9f2072769d0
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2023-06-14 00:44:57 -0400

    LoadingIndicator abstraction

---
+++ core/src/main/scala/io/github/nafg/scalajs/react/util/LoadingIndicator.scala
@@ -0,0 +1,20 @@
+package io.github.nafg.scalajs.react.util
+
+import japgolly.scalajs.react.vdom.VdomNode
+
+
+trait LoadingIndicator {
+  def render: VdomNode
+}
+object LoadingIndicator {
+  def apply(node: => VdomNode) = new LoadingIndicator {
+    override def render = node
+  }
+
+  val spinner10 = LoadingIndicator(Spinner.inline(10))
+  val spinner16 = LoadingIndicator(Spinner.inline(16))
+
+  object Implicits {
+    implicit def spinner16: LoadingIndicator = LoadingIndicator.spinner16
+  }
+}
