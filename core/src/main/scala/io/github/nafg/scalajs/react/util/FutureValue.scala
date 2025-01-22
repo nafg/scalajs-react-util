af400c337c5a694289bac0409beb08e3fb3368e8
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2025-01-22 04:32:31 -0500

    FutureValue.useQueryForgetful

--- core/src/main/scala/io/github/nafg/scalajs/react/util/FutureValue.scala
+++ core/src/main/scala/io/github/nafg/scalajs/react/util/FutureValue.scala
@@ -5,6 +5,7 @@ import scala.util.{Failure, Success, Try}
 
 import org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits.global
 import japgolly.scalajs.react.hooks.CustomHook
+import japgolly.scalajs.react.hooks.Hooks
 import japgolly.scalajs.react.vdom.html_<^.*
 import japgolly.scalajs.react.{AsyncCallback, Reusability}
 
@@ -27,17 +28,40 @@ case class FutureValue[A](value: Option[Try[A]] = None) {
 object FutureValue {
   private implicit val reuseThrowable: Reusability[Throwable]                = Reusability.by_==
   private implicit def reuseTry[A: Reusability]: Reusability[Try[A]]         = Reusability.by(_.toEither)
-  implicit def reuseFutureValue[A: Reusability]: Reusability[FutureValue[A]] =
-    Reusability.by(_.value)
+  implicit def reuseFutureValue[A: Reusability]: Reusability[FutureValue[A]] = Reusability.by(_.value)
+
+  private def make[A](state: Hooks.UseState[FutureValue[A]], future: => Future[A]): AsyncCallback[Unit] =
+    AsyncCallback
+      .fromFuture(future)
+      .attemptTry
+      .flatMapSync(result => state.modState(_.copy(value = Some(result))))
 
   def useQuery[D: Reusability, A](dep: D)(query: D => Future[A]) =
     CustomHook[Unit]
       .useState(FutureValue[A]())
       .useEffectWithDepsBy((_, _) => dep) { (_, state) => dep =>
-        AsyncCallback
-          .fromFuture(query(dep))
-          .attemptTry
-          .flatMapSync(result => state.modState(_.copy(value = Some(result))))
+        make(state, query(dep))
+      }
+      .buildReturning((_, state) => state.value)
+
+  /** A custom hook that executes a query based on a dependency and manages the result as a `FutureValue`. The result of
+    * the query is kept in the hook's state and updated whenever the dependency changes.
+    *
+    * Unlike [[useQuery]], whenever the dependency changes, the FutureValue is reset to None (pending) until the new
+    * query execution completes.
+    *
+    * @param dep
+    *   A dependency value of type `D` that is tracked for changes.
+    * @param query
+    *   A function that takes a dependency of type `D` and returns a `Future[A]`.
+    */
+  def useQueryForgetful[D: Reusability, A](dep: D)(query: D => Future[A]) =
+    CustomHook[Unit]
+      .useState(FutureValue[A](None))
+      .useEffectWithDepsBy((_, _) => dep) { case ((), state) =>
+        dep =>
+          state.setState(FutureValue(None)).asAsyncCallback >>
+            make(state, query(dep))
       }
       .buildReturning((_, state) => state.value)
 }
